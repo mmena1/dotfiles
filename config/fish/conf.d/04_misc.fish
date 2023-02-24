675658ef946aad24f9258799caa5131ceb3cda9e
Martin Mena <martinmena@outlook.com>
2023-02-24 14:04:39 -0500

    Add .local/bin to PATH

--- config/fish/conf.d/04_misc.fish
+++ config/fish/conf.d/04_misc.fish
@@ -1,5 +1,7 @@
 
 set -gx DOTFILES "$HOME/.dotfiles"
+set -gx PATH="$HOME/.local/bin:$PATH"
+
 # Run scripts
 abbr update "source $DOTFILES/scripts/update"
 abbr bootstap "source $DOTFILES/scripts/bootstrap"
