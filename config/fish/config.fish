4fe66e3fcc7601744a6c31e3f75024a01877ca42
Martin Mena <martinmena@outlook.com>
2023-06-23 09:52:25 -0500

    Update config

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -2,3 +2,6 @@ if status is-interactive
     # Set python debugger to ipdb - on fish it must be set with `sset_trace`
     set -x PYTHONBREAKPOINT ipdb.sset_trace
 end
+
+# Haskell setup
+set -q GHCUP_INSTALL_BASE_PREFIX[1]; or set GHCUP_INSTALL_BASE_PREFIX $HOME ; set -gx PATH $HOME/.cabal/bin /home/mmena/.ghcup/bin $PATH # ghcup-env
