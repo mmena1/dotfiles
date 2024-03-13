17a72fcf72c2406870b225ceaf47577b770d957b
Martin Mena <mmena@twilio.com>
2024-03-12 21:52:45 -0500

    Improve y/n dialog input and clean some stuff

--- home/private_dot_config/private_fish/conf.d/04_misc.fish
+++ home/private_dot_config/private_fish/conf.d/04_misc.fish
@@ -1,10 +1,6 @@
 
-set -gx DOTFILES "$HOME/.dotfiles"
+set -gx DOTFILES "$HOME/.local/share/chezmoi"
 set -gx PATH "$HOME/.local/bin:$PATH"
-
-# Run scripts
-abbr update "source $DOTFILES/scripts/update"
-abbr bootstap "source $DOTFILES/scripts/bootstrap"
 
 # Quick jump to dotfiles
 abbr dotfiles "code $DOTFILES"
